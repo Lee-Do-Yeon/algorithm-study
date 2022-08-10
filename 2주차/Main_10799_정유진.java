bbe6c2baf3df035f07727fc92cdcc88e2ff28b85
KathleenJung <85939045+KathleenJung@users.noreply.github.com>
2022-08-10 23:27:01 +0900

    Add files via upload

---
+++ 2주차/Main_10799_정유진.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main_10799_정유진 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String input = bf.readLine();
+		Stack<Character> stack = new Stack<>();
+
+		int ans = 0;
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == '(') {
+				stack.push('(');
+//				continue;
+			}
+			if (input.charAt(i) == ')') {
+				stack.pop();
+
+				if (input.charAt(i - 1) == '(') {
+					ans += stack.size();
+				} else {
+					ans++;
+				}
+			}
+		}
+
+		System.out.println(ans);
+
+	}
+}
