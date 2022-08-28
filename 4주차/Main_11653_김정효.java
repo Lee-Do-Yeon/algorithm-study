752d09c0f88ade055e503772647f8834935562ba
hyooo-o <72725382+hyooo-o@users.noreply.github.com>
2022-08-29 00:03:54 +0900

    08/24(수)_소인수분해

---
+++ 4주차/Main_11653_김정효.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main_11653_김정효 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<Integer>();
+		
+		if (n == 1) return;
+		while(n != 1) {
+			for (int i = 2; i <= n; i++) {
+				if (n % i == 0 && check(i)) {	// 소수이면서 n의 약수이면
+					n /= i;
+					list.add(i);
+					continue;
+				}
+			}
+		}
+		Collections.sort(list);
+		for (int l : list) {
+			System.out.println(l);
+		}
+	}
+
+	private static boolean check(int i) {
+		for (int j = 2; j < i; j++) {
+			if (i % j == 0) return false;
+		}
+		return true;
+	}
+
+}
