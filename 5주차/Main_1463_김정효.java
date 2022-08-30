597a04b9b70df41ce6a79fea86cf7a048a27ad4c
hyooo-o <72725382+hyooo-o@users.noreply.github.com>
2022-08-30 22:12:10 +0900

    08/29(월)_1로 만들기 - edit

--- 5주차/Main_1463_김정효.java
+++ 5주차/Main_1463_김정효.java
@@ -4,40 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 /**
- * BOJ 1463 1로 만들기  (미완성)
- * 모르겠다..
+ * BOJ 1463 1로 만들기  (완성)
+ * DF로도 풀이 가능
  * @author kjh
  *
  */
 public class Main_1463_김정효 {
 
-	static int _min;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int x = Integer.parseInt(br.readLine());
-		_min = Integer.MAX_VALUE;
-		bfs(x, 0);
-		System.out.println(_min);
+		
+		System.out.println(bfs(x, 0));
 	}
 
-	private static void bfs(int n, int cnt) {
-		if(n == 1) {
-			_min = Math.min(_min, cnt);
-			return;
+	private static int bfs(int n, int cnt) {
+		if(n < 2) {	// n == 1 로하면 오류남   =>  1이 아니라 0으로 끝날 수도 있기 때문에 2보다 작다고 설정해주어야 함
+			return cnt;
 		}
-		
-		if(n % 3 == 0) {
-			bfs(n/3, cnt+1);
-		}else {
-			bfs(n-1, cnt+1);
-		}
-		
-		if(n % 2 == 0) {
-			bfs(n/2, cnt+1);
-		}else {
-			bfs(n-1, cnt+1);
-		}
-		
+		// bfs(n/2, cnt+1+(n%2)) : 2로 나누었으니깐 cnt 횟수를 1 늘려주고, 2로 나누었을 때의 나머지는 빼기 1를 한 것과 같으므로 나머지를 cnt 횟수에 더해줌
+		// bfs(n/3, cnt+1+(n%3)) : 3로 나누었으니깐 cnt 횟수를 1 늘려주고, 3로 나누었을 때의 나머지는 빼기 1를 한 것과 같으므로 나머지를 cnt 횟수에 더해줌
+		// 둘 중 최소값을 return 해줌
+		return Math.min(bfs(n/2, cnt+1+(n%2)), bfs(n/3, cnt+1+(n%3)));
+		 
 	}
 
 }
